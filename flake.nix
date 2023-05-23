cba5f2b6b9b9c667860b29b6f8954f7c9de7306a
Ingo Ruhnke <grumbel@gmail.com>
2023-05-23 21:06:01 +0200

    Update flake.nix to nixos-23.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Play it slowly is a software to play back audio files at a different speed or pitch";
 
   inputs = rec {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
